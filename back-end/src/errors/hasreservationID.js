1270a70d787e56a93fe1df01d64084440e44a2d8
Alex Goode <alexgoodestudio@gmail.com>
2023-10-16 17:07:51 -0400

    fix form

--- back-end/src/errors/hasreservationID.js
+++ back-end/src/errors/hasreservationID.js
@@ -1,17 +1,17 @@
 function hasReservationID(req, res, next) {
-    // console.log('hasReservationID Middleware Invoked'); // debugging
-    // console.log('Request Params:', req.params); // debugging
-    // console.log('Request Body:', req.body); // debugging
+    console.log('hasReservationID Middleware Invoked'); 
+    console.log('Request Params:', req.params); 
+    console.log('Request Body:', req.body); 
   
     const reservation_id = req.params.reservation_id || req.body?.data?.reservation_id;
   
-    // console.log('Resolved reservation_id:', reservation_id); // debugging
+    console.log('Resolved reservation_id:', reservation_id); 
   
     if (reservation_id) {
       res.locals.reservation_id = reservation_id;
       next();
     } else {
-      console.error('Missing reservation_id'); // debugging
+      console.error('Missing reservation_id'); 
       next({
         status: 400,
         message: 'missing reservation_id',
